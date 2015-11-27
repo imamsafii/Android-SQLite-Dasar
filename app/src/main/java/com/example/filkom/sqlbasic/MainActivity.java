a65586a157d225bc4136410596e84ed468e9eab7
imaammdev <imaamm.dev@gmail.com>
2015-11-27 13:37:32 +0700

    mengubah listarray tanpa final

--- app/src/main/java/com/example/filkom/sqlbasic/MainActivity.java
+++ app/src/main/java/com/example/filkom/sqlbasic/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends Activity {
         dataTamu = new DataSourceTamu(this);
         dataTamu.open();
 
-        List<Tamu> values = dataTamu.semuaTamu();
+        final List<Tamu> values = dataTamu.semuaTamu();
         adapter = new ArrayAdapter<Tamu>(this,
                 android.R.layout.simple_list_item_1, values);
 
